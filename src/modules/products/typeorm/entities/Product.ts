9bb0aa69ce42d457bf1d41d84b1b463406bef4fb
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-08-14 16:37:52 -0300

    CRUD Products

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  brand: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @Column('int')
+  minimal_quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
---
+++ src/shared/typeorm/migrations/1628904337512-CreateProducts.ts
@@ -0,0 +1,60 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProducts1628904337512 implements MigrationInterface {
+
+public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {  
+            name: 'description',
+            type: 'varchar'
+          },
+          {  
+            name: 'brand',
+            type: 'varchar'
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2
+          },
+          {
+            name: 'quantity',
+            type: 'int'
+          },
+          {
+            name: 'minimal_quantity',
+            type: 'int'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+        ],
+      }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('products');
+    }
+
+}
